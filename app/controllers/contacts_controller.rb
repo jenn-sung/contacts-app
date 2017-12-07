699d131f7437655ecdda784cdac2d8477e4ee64c
JennSung <jennifer.sung@colorado.edu>
2017-12-06 22:06:30 -0700

    adding of friendly

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,10 +2,11 @@ class  V1::ContactsController < ApplicationController
   
   def contact1
     contact1 = Contact.first
-      render json: contact1.as_json 
+    render json: contact1.as_json 
   end
 
   def index
+    contacts =
 
-    
+
   end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,17 @@
 class Contact < ApplicationRecord
+
+  def as_json
+    {
+    first_name: first_name,
+    last_name: last_name,
+    email: email, 
+    phone: phone
+    }
+  end
+
+  def full_name
+
+    
+  end
+
 end
